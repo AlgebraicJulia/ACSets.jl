c9394149c5dc809a00ef2d87d05d27a56e1d7f2e
Evan Patterson <evan@epatters.org>
2023-10-11 11:01:02 -0700

    CLEANUP: Communicate with Nauty using Julia process API.

--- ext/ACSetsnauty_jllExt.jl
+++ ext/ACSetsnauty_jllExt.jl
@@ -3,20 +3,20 @@ module ACSetsnauty_jllExt
 using ACSets
 using nauty_jll
 
-bashit(str) = run(`bash -c "$str"`)
-
 """Compute CSetNautyRes from an ACSet."""
-ACSets.call_nauty(g::ACSet)::CSetNautyRes = 
+function ACSets.call_nauty(g::ACSet)::CSetNautyRes
   ACSets.NautyInterface.parse_res(nauty_res(g), g)
+end
 
 """Make shell command to dreadnaut (nauty) and collect stdout text."""
-function nauty_res(g::T)::String where T<:ACSet
-  if isempty(g) return NautyRes(g) end
-  all(o -> nparts(g, o) == 0, attrtypes(acset_schema(g))
-     ) || error("VarACSets not yet supported")
-  tmp = tempname()
-  bashit("echo \"$(ACSets.NautyInterface.dreadnaut(g))\" | $(nauty_jll.dreadnaut_path) > $tmp")
-  return open(f->read(f, String), tmp)
+function nauty_res(g::ACSet)::AbstractString
+  isempty(g) && return NautyRes(g)
+  all(o -> nparts(g, o) == 0, attrtypes(acset_schema(g))) ||
+    error("Nauty integration with VarACSets not yet supported")
+  process = open(`$(nauty_jll.dreadnaut_path)`, write=true, read=true)
+  print(process, ACSets.NautyInterface.dreadnaut(g))
+  close(process.in)
+  read(process, String)
 end
 
 end # module
