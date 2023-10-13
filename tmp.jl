c84c4c9ec4706e0cced1777f5ab2f2b0f84787b5
slwu89 <slwu89@users.noreply.github.com>
2023-10-12 22:02:31 -0700

    notes on updating cache when parts/subparts change

--- tmp.jl
+++ tmp.jl
@@ -13,80 +13,80 @@ MySch = BasicSchema(
 
 @acset_type MyDataType(MySch, index=[:proj_x,:proj_y], unique_index=[:xname,:yname])
 
-mydata = @acset MyDataType{Symbol,Float64} begin
-    X = 4
-    Y = 3
-    xname = [:widget1,:widget2,:widget3,:widget4]
-    yname = [:place1,:place2,:place3]
-end
+# mydata = @acset MyDataType{Symbol,Float64} begin
+#     X = 4
+#     Y = 3
+#     xname = [:widget1,:widget2,:widget3,:widget4]
+#     yname = [:place1,:place2,:place3]
+# end
 
-costs = rand(4,3)
+# costs = rand(4,3)
 
-add_parts!(
-    mydata, :Rel, prod(size(costs)),
-    proj_x = repeat(1:4,3),
-    proj_y = vcat(map(i->fill(i,4),1:3)...),
-    cost = vcat(costs...)
-)
+# add_parts!(
+#     mydata, :Rel, prod(size(costs)),
+#     proj_x = repeat(1:4,3),
+#     proj_y = vcat(map(i->fill(i,4),1:3)...),
+#     cost = vcat(costs...)
+# )
 
-# a pattern that comes up very, very often
-mydata[
-    intersect(
-        incident(mydata, :widget3, [:proj_x,:xname]),
-        incident(mydata, :place2, [:proj_y,:yname])
-    ),
-    :cost
-]
+# # a pattern that comes up very, very often
+# mydata[
+#     intersect(
+#         incident(mydata, :widget3, [:proj_x,:xname]),
+#         incident(mydata, :place2, [:proj_y,:yname])
+#     ),
+#     :cost
+# ]
 
-# Kris' example
-SchTest = BasicSchema(
-    [:A,:B,:C,:R],
-    [(:f,:R,:A),(:g,:R,:B),(:h,:R,:C)],
-    [],
-    []
-)
+# # Kris' example
+# SchTest = BasicSchema(
+#     [:A,:B,:C,:R],
+#     [(:f,:R,:A),(:g,:R,:B),(:h,:R,:C)],
+#     [],
+#     []
+# )
 
-# @acset_type Test(SchTest, index=[:h, (:f,:g)])
-@acset_type DataTest(SchTest)
+# # @acset_type Test(SchTest, index=[:h, (:f,:g)])
+# @acset_type DataTest(SchTest)
 
-X = @acset DataTest begin
-    A=1
-    B=1
-    C=1
-    R=1
-    f=[1]
-    g=[1]
-    h=[1]
-end
+# X = @acset DataTest begin
+#     A=1
+#     B=1
+#     C=1
+#     R=1
+#     f=[1]
+#     g=[1]
+#     h=[1]
+# end
 
-# incident(X, (1,1), (:f,:g)) # == [1]
-incident(X, 1, :f)
-incident(X, 1, :g)
+# # incident(X, (1,1), (:f,:g)) # == [1]
+# incident(X, 1, :f)
+# incident(X, 1, :g)
 
-# acs = X
-# parts = (1,1)
-# f = (:f,:g)
+# # acs = X
+# # parts = (1,1)
+# # f = (:f,:g)
 
-function ACSetInterface.incident(
-        acs::SimpleACSet, parts::T1, f::T2) where {T1<:Tuple{Vararg{<:Integer}},T2<:Tuple{Vararg{Symbol}}}
-    s = acset_schema(acs)
-    length(unique([dom(s,f[i]) for i in eachindex(f)])) == 1 || error("some homs in $(f) do not have the same domain")
-    length(parts) == length(f) || error("parts should be the same length as f")
-    return intersect([incident(acs, parts[i], f[i]) for i in eachindex(f)]...)
-end
+# function ACSetInterface.incident(
+#         acs::SimpleACSet, parts::T1, f::T2) where {T1<:Tuple{Vararg{<:Integer}},T2<:Tuple{Vararg{Symbol}}}
+#     s = acset_schema(acs)
+#     length(unique([dom(s,f[i]) for i in eachindex(f)])) == 1 || error("some homs in $(f) do not have the same domain")
+#     length(parts) == length(f) || error("parts should be the same length as f")
+#     return intersect([incident(acs, parts[i], f[i]) for i in eachindex(f)]...)
+# end
 
 
-incident(X, (1,1), (:f,:g))
+# incident(X, (1,1), (:f,:g))
 
-function ACSetInterface.incident(
-        acs::SimpleACSet, parts::T1, f::T2) where {T1<:Tuple,T2<:Tuple{Vararg{AbstractVector{Symbol}}}}
-    s = acset_schema(acs)
-    length(unique([dom(s,f[i][1]) for i in eachindex(f)])) == 1 || error("some homs in $(f) do not have the same domain")
-    length(parts) == length(f) || error("parts should be the same length as f")
-    return intersect(map(i->incident(acs, parts[i], f[i]), eachindex(f))...)
-end
+# function ACSetInterface.incident(
+#         acs::SimpleACSet, parts::T1, f::T2) where {T1<:Tuple,T2<:Tuple{Vararg{AbstractVector{Symbol}}}}
+#     s = acset_schema(acs)
+#     length(unique([dom(s,f[i][1]) for i in eachindex(f)])) == 1 || error("some homs in $(f) do not have the same domain")
+#     length(parts) == length(f) || error("parts should be the same length as f")
+#     return intersect(map(i->incident(acs, parts[i], f[i]), eachindex(f))...)
+# end
 
-incident(mydata, (:widget3,:place2), ([:proj_x,:xname],[:proj_y,:yname]))
+# incident(mydata, (:widget3,:place2), ([:proj_x,:xname],[:proj_y,:yname]))
 
 # does the new incident with composed arrows work with the composed arrows are of diff lengths?
 # e.g. z -> x -> y
@@ -109,9 +109,9 @@ X_set = FinSet(nparts(rel_redundant,:X))
 Y_set = FinSet(nparts(rel_redundant,:Y))
 Z_prod = Catlab.product(X_set, Y_set)
 
-Z_ob = apex(rel_prod)
-Z_f = legs(rel_prod)[1]
-Z_g = legs(rel_prod)[2]
+Z_ob = apex(Z_prod)
+Z_f = legs(Z_prod)[1]
+Z_g = legs(Z_prod)[2]
 
 add_parts!(
     rel_redundant, 
@@ -163,7 +163,6 @@ add_parts!(
 # multicolumn index being computed on the fly
 
 function multicol_incident_nocache(acs, parts, f)
-    s = acset_schema(acs)
     return intersect([incident(acs, parts[i], f[i]) for i in eachindex(f)]...)
 end
 
@@ -213,8 +212,13 @@ end
 
 for acs in [rel_redundant,rel_product,rel_sparse]
     cache = make_cache_rel(acs, (:proj_x,:proj_y))
-
+    for (i,j) in Iterators.product(parts(rel_redundant,:X), parts(rel_redundant,:Y))
+        rel_parts_cache = cache[(i,j)]
+        rel_parts_inc = intersect(incident(acs, i, :proj_x), incident(acs, j, :proj_y))
+        @test rel_parts_cache == rel_parts_inc
+    end
 end
+
 
 
 # --------------------------------------------------------------------------------
@@ -236,4 +240,16 @@ end
 
 
 # --------------------------------------------------------------------------------
-# obvious to-do's ... updating the cache after adding/rem parts.
+# obvious to-do's ... updating the cache after adding/rem parts
+
+# 1. adding parts involved in a cached span
+# 1a. part is to the apex: if the subparts were set, find which key the new part points to and add its index to 
+#     the existing values (indices) at that key
+#     NOTE: actually maybe all this logic should only be done at set_subparts! time, because if
+#     the user didnt set all the subparts for this part, the preimage won't exist
+# 1b. part is in a leg. Nothing to do, because the relevant subparts are only associated with the apex (domain)
+# 
+# 2. removing parts involved in a cached span
+# 2a. part is in the apex, subparts will update after deletion...probably need to recache the values (indices)
+#     but at least the keys are the same
+# 2b. part is in legs, uh oh, the keys changed. Everything has to be redone.
