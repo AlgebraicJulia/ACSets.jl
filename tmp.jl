f4481c7aa53d92a4cd01144805b5ec28de0ff796
slwu89 <10673535+slwu89@users.noreply.github.com>
2023-10-17 15:26:36 -0700

    fix unmerged files

--- tmp.jl
+++ tmp.jl
@@ -33,3 +33,54 @@ mydata[
     ),
     :cost
 ]
+
+# Kris' example
+using Catlab
+SchTest = BasicSchema(
+    [:A,:B,:C,:R],
+    [(:f,:R,:A),(:g,:R,:B),(:h,:R,:C)],
+    [],
+    []
+)
+
+# @acset_type Test(SchTest, index=[:h, (:f,:g)])
+@acset_type Test(SchTest)
+
+X = @acset Test begin
+    A=1
+    B=1
+    C=1
+    R=1
+    f=[1]
+    g=[1]
+    h=[1]
+end
+
+# incident(X, (1,1), (:f,:g)) # == [1]
+incident(X, 1, :f)
+incident(X, 1, :g)
+
+acs = X
+parts = (1,1)
+f = (:f,:g)
+
+function ACSetInterface.incident(
+        acs::SimpleACSet, parts::T1, f::T2) where {T1<:Tuple,T2<:Tuple{Vararg{Symbol}}}
+    s = acset_schema(acs)
+    length(unique([dom(s,f[i]) for i in eachindex(f)])) == 1 || error("some homs in $(f) do not have the same domain")
+    length(parts) == length(f) || error("parts should be the same length as f")
+    return intersect([incident(acs, parts[i], f[i]) for i in eachindex(f)]...)
+end
+
+
+incident(X, (1,1), (:f,:g))
+
+function ACSetInterface.incident(
+        acs::SimpleACSet, parts::T1, f::T2) where {T1<:Tuple,T2<:Tuple{Vararg{AbstractVector{Symbol}}}}
+    # foldr(names, init=part) do name, part
+    #     reduce(vcat, collect.(incident(acs, part, name)), init=Int[])
+    # end
+    println("im being called!")
+end
+
+incident(mydata, (:widget3,:place2), ([:proj_x,:xname],[:proj_y,:yname]))
