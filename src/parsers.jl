7e7c072e52a751a73dbfef48ae578d6415f49202
Christian Scaff <cscaff@ufl.edu>
2024-09-17 12:09:12 -0400

    Created Parsers.jl to contain ACSetSpec PEG Implementation

---
+++ src/parsers.jl
@@ -0,0 +1,14 @@
+""" Parsers
+
+Test case displaying parsing of ACSetSpecs using PEG.jl.
+This module also includes basic lexing rules as seen in
+SyntacticModels.jl.
+
+"""
+
+module Parsers
+
+@reexport using PEG
+
+
+end
