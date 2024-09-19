96359f1c404d462dc0c3730ef2de9ab8caa89d58
Christian Scaff <cscaff@ufl.edu>
2024-09-18 23:10:10 -0400

    Transformations added except @args transformation. @Arg appears to be working, yet its test are failing...

--- src/parsers.jl
+++ src/parsers.jl
@@ -9,6 +9,8 @@ SyntacticModels.jl.
 module Parsers
 
 using Reexport
+using ACSets, ACSets.ADTs
+
 @reexport using PEG
 
 #Export lexing rules
@@ -26,7 +28,7 @@ export body, block, line, statement, args, arg
 @rule comma = r","p
 @rule EOL = "\n" , ";"
 @rule colon = r":"p
-@rule identifier = r"[^:{}→\n;=,\(\)]*"
+@rule identifier = r"[^:{}→\n;=,\(\)\s]+"
 
 # Core Parsing rules for ACSetSpecs
 # ACSetSpec Structure:
@@ -45,26 +47,23 @@ export body, block, line, statement, args, arg
 # This PEG.jl based parser takes from the recursive decent
 # parser in ACSets.jl/ADTs.jl and parses the body in "acsetspec(head, body)"
 
-@rule body = r"quote" & block & r"end"p
-@rule block = line[*] & r"\n?"p
-@rule line  = ws & statement & ws & EOL
-@rule statement = identifier & lparen & args & rparen
-@rule args = (arg & comma)[*] & arg
-@rule arg = (identifier & eq & args) , identifier
-
-#Deleted args rule for stack overflow problem
-# @rule arg = args ,  (identifier & eq & args) , identifier creates stack overflow
-
-
-
-# Body contains "qoute ... end", containing a block of code
+@rule body = r"quote" & block & r"end"p |> v -> v[2]
 # Block contains one or more lines of statements
+@rule block = line[*] & r"\n?"p |> v -> v[1]
 # Line contians a statement followed by a new line or ";"
+@rule line  = ws & statement & ws & EOL |> v -> v[2]
 # Statement contains a call followed by arguments in parenthesis: "identifier(args)"
+@rule statement = identifier & lparen & args & rparen |> Statement(Symbol(v[1]), v[3])
 # args contains one or more arguments separated by commas
-# arg contains multiple possibilites:
-#   - A list of further arguments
-#   - A key followed by an equals sign and a value: "identifier = args"
-#   - A single value
+@rule args = (arg & comma)[*] & arg |> v -> collect_args(v)
+# arg can be a list of further arguments, a key-value pair, or a single value
+@rule arg = (lparen & args & rparen) |> v -> v[2], 
+            ((identifier & eq & arg) |> v -> Kwarg(Symbol(v[1]), v[3])), 
+            ((identifier) |> v -> Value(v[1]))
+
+#Collects and flattens arguments into a single list
+collect_args(args) = begin
+    return 0 #Needs to be implemented
+end
 
 end
--- test/parsers.jl
+++ test/parsers.jl
@@ -5,9 +5,10 @@ using ACSets
 using ACSets.ADTs
 using ACSets.Parsers
 
-#Testing arg rule before applied transformation
-@testset "arg" begin
-    @test arg("1")[1] == "1"
+@testset "args" begin
+    @test arg("1")[1] == Value(1) #Fails but appears to be correct
+    @test arg("test=2")[1] == Kwarg(:test, Value(2)) #Fails but appears to be correct
+    #@test arg("(1, 2, 3)") #Collection has not been implemented yet
 end
 
 end
