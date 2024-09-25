66037ec9d35085edc2d6cb49ef2199643ff93e4d
Christian Scaff <cscaff@ufl.edu>
2024-09-25 18:44:44 -0400

    Created string macro and full unit test case.

--- src/parsers.jl
+++ src/parsers.jl
@@ -8,7 +8,7 @@ Tasks Left to Complete:
 
 - Add more test cases for the parser
 - Call parser from ADTs.jl acsetspec function
-- Add error handling for invalid input
+- Add error handling for invalid input?
 - Enable extraction from files?
 
 """
@@ -20,15 +20,17 @@ using ACSets, ACSets.ADTs
 
 @reexport using PEG
 
+#Export macro
+export @acsetspec_str
 #Export lexing rules
 export ws, eq, lparen, rparen, comma, EOL, colon, identifier
 #Export parsing rules
-export body, block, line, statement, args, arg
+export acset_spec, block, line, statement, args, arg
 
 # Basic Lexing rules for scanning sting of characters
 # Breaks up words/structures into tokens
 
-@rule ws = r"[^\S\r\n]*"
+@rule ws = r"\s*"
 @rule eq = r"="p
 @rule lparen = r"\("
 @rule rparen = r"\)"
@@ -42,27 +44,33 @@ export body, block, line, statement, args, arg
 # acsetspec(head, body)
 # Example:
 #
-# acsetspec(:(LabeledGraph{Symbol}),quote
-#       V(label=a)
-#       V(label=b)
-#       V(label=c)
-#       E(src=1,tgt=3)
-#       E(src=2,tgt=3)
-#   end
-#   )
+# acsetspec"""
+# LabeledGraph{Symbol}
+# begin
+#        V(label=a)
+#        V(label=b)
+#        V(label=c)
+#        E(src=1,tgt=3)
+#        E(src=2,tgt=3)
+# end
+# """
 #
-# This PEG.jl based parser takes from the recursive decent
-# parser in ACSets.jl/ADTs.jl and parses the body in "acsetspec(head, body)"
 
-@rule body = r"quote\s*"p & block & r"end"p |> v -> v[2]
+# This PEG.jl based parser takes from the recursive decent
+# parser in ACSets.jl/ADTs.jl and parses "acsetspec(head, body)"
+
+#acset_spec takes in head and body args
+@rule acset_spec = ws & head & r"begin"p & block & r"end"p |> v -> ACSetSpec(v[2], v[4])
+#Head contains ACsetSpec Type
+@rule head = r"\S*"p |> v -> Symbol(v[2])
 # Block contains one or more lines of statements
 @rule block = line[*] & r"\n?"p |> v -> v[1]
 # Line contians a statement followed by a new line or ";"
-@rule line  = ws & statement & ws & EOL |> v -> v[2]
+@rule line  = ws & statement & r"[^\S\r\n]*" & EOL |> v -> v[2]
 # Statement contains a call followed by arguments in parenthesis: "identifier(args)"
 @rule statement = identifier & lparen & args & rparen |> v -> Statement(Symbol(v[1]), v[3])
 # args contains one or more arguments separated by commas
-@rule args = (arg & comma)[*] & arg |> v -> collect_args(v)
+@rule args = (arg & ws & comma)[*] & arg |> v -> collect_args(v)
 # arg can be a list of further arguments, a key-value pair, or a single value
 @rule arg = ((lparen & args & rparen) |> v -> v[2]), 
             ((identifier & eq & arg) |> v -> Kwarg(Symbol(v[1]), v[3])), 
@@ -82,5 +90,8 @@ parse_identifier(x) = begin
         return Value(Symbol(x))
     end
 end
+
+#Creates a string macro to parse/create acsetspec
+macro acsetspec_str(x::String) parse_whole(acset_spec, x) end
 
 end
--- test/parsers.jl
+++ test/parsers.jl
@@ -1,9 +1,10 @@
 # ## Testing our parser
 module ParserTests
+
 using Test
-using ACSets
-using ACSets.ADTs
+using ACSets, ACSets.ADTs
 using ACSets.Parsers
+
 
 #Overaloads "==" to properly comapre two statement structs
 function Base.:(==)(s1::ACSets.ADTs.Statement, s2::ACSets.ADTs.Statement)
@@ -23,7 +24,7 @@ function parse_fails_at(rule, input)
     end
   end
 
-#Test Groups per rule:
+# -------------- Test Groups per rule: -------------- #
 @testset "arg_test" begin
     @test arg("1")[1] == Value(1)
     @test arg("test=2")[1] == Kwarg(:test, Value(2))
@@ -33,6 +34,7 @@ end
 @testset "args_test" begin
     @test args("a, b, c")[1] == ([Value(:a), Value(:b), Value(:c)])
     @test args("1,2,3")[1] == ([Value(1), Value(2), Value(3)])
+    @test args("1 , 1")[1] == ([Value(1), Value(1)])
     @test args("a=1, b=2, c=3")[1] == ([Kwarg(:a, Value(1)), Kwarg(:b, Value(2)), Kwarg(:c, Value(3))])
 end
 
@@ -53,24 +55,9 @@ end
     @test block("test(a)\n test(b)\n test(c)\n")[1] == [Statement(:test, [Value(:a)]), Statement(:test, [Value(:b)]), Statement(:test, [Value(:c)])]
 end
 
-@testset "full_body_test" begin
-    @test body("quote
-      V(label=a)
-      V(label=b)
-      V(label=c)
-      E(src=1,tgt=3)
-      E(src=2,tgt=3)
-  end")[1] ==  [
-    Statement(:V, [Kwarg(:label, Value(:a))])
-    Statement(:V, [Kwarg(:label, Value(:b))])
-    Statement(:V, [Kwarg(:label, Value(:c))])
-    Statement(:E, [Kwarg(:src, Value(1)), Kwarg(:tgt, Value(3))])
-    Statement(:E, [Kwarg(:src, Value(2)), Kwarg(:tgt, Value(3))])
-    ]
-end
 
+# -------------- Test Error Handling -------------- #
 
-#Test Error Handling
 @testset "arg_handling" begin
    @test parse_fails_at(arg, "(1") == 3 #Missing "(" at index 3
    @test parse_fails_at(arg, "a=") == 3 #Missing value after "=" at index 3
@@ -80,11 +67,8 @@ end
 @testset "args_handling" begin
     @test parse_fails_at(args, "1,") == 3 #Missing value after "," at index 3
     @test parse_fails_at(args, ", 3") == 1 #Missing initial value before "," at index 1
-    @test parse_fails_at(args, "1  1") == 2 #Missing "," at index 2
+    @test parse_fails_at(args, "1  1") == 4 #Missing "," at index 4
 end
-
-#Found Issue:
-#    @test parse_fails_at(args, "1 , 1") == 2 #Missing "," at index 2. Should ws be allowed between commas?
 
 @testset "statement_handling" begin
     @test parse_fails_at(statement, "test") == 5 #Missing "(" at index 5
@@ -102,9 +86,49 @@ end
     @test parse_fails_at(block, "test(a) test(b)") == 9 #Missing EOL at index 9
 end
 
-@testset "body_handling" begin
-    @test parse_fails_at(body, "quote\ntest(a)\n") == 15 #Missing "end" at index 15
-    @test parse_fails_at(body, "quote\n") == 7 #Missing "end" at index 7
+@testset "acset_spec_handling" begin
+    @test parse_fails_at(acset_spec, "begin\ntest(a)\ntest(b)\nend") == 7
+    # Missing head at index 1, however, parser will try and create a head until
+    # it reaches a white space at index 6. This can be modified for better parsing errors.
+    # However, for the simplicity of the grammar, I'm keeping it as is for now.
+    # Right now, it registers begin as a head and fails due to the true "begin" missing.
+end
+
+# ------------ Full Scale Tests ----------- #
+
+PEG.setdebug!(false)
+
+SchLabeledGraph = BasicSchema([:E,:V], [(:src,:E,:V),(:tgt,:E,:V)],
+                          [:L], [(:label,:V,:L)])
+
+@acset_type LabeledGraph(SchLabeledGraph, index=[:src,:tgt])
+
+@testset "Constructing ACSets from Specs" begin
+    gspec = ACSetSpec(
+      :(LabeledGraph{Symbol}),
+      [
+          Statement(:V, [Kwarg(:label, Value(:a))])
+          Statement(:V, [Kwarg(:label, Value(:b))])
+          Statement(:V, [Kwarg(:label, Value(:c))])
+          Statement(:E, [Kwarg(:src, Value(1)), Kwarg(:tgt, Value(3))])
+          Statement(:E, [Kwarg(:src, Value(2)), Kwarg(:tgt, Value(3))])
+      ]
+    )
+    g = construct(LabeledGraph{Symbol}, gspec)
+    @test nparts(g, :V) == 3
+    @test nparts(g, :E) == 2
+
+    hspec = acsetspec"""
+    LabeledGraph{Symbol}
+    begin
+      V(label=a)
+      V(label=b)
+      V(label=c)
+      E(src=1,tgt=3)
+      E(src=2,tgt=3)
+    end"""
+
+    @test construct(LabeledGraph{Symbol}, hspec) == construct(LabeledGraph{Symbol}, gspec)
 end
 
 end
