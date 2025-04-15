48e7907d5695b2a87a5b2369b3b2d485c443263c
Matt Cuffaro <cuffaro.matt@gmail.com>
2025-04-15 15:26:42 -0400

    using @something

--- src/serialization/CCLACSets.jl
+++ src/serialization/CCLACSets.jl
@@ -3,13 +3,13 @@ export read_ccl_acset
 
 using ...ACSetInterface, ...Schemas, ..ACSetSerialization
 
-@kwdef struct CCLSpec end
+using JSON3
+using MLStyle
 
-@data _CCL begin
-    TheModel(name::Symbol, elem::Dict{String, ModelElement})
-    ThDiagram
+@data AnElement begin
+    ObjectElem(name::String)
+    HomElem(name::String, dom::String, cod::String)
 end
-
 
 struct CCLModel
     name::Symbol
@@ -19,11 +19,6 @@ end
 struct CCLDiagram
     name::Symbol
     elem::Dict{String, AnElement}
-end
-
-@data AnElement begin
-    ObjectElem(name::String)
-    HomElem(name::String, dom::String, cod::String)
 end
 
 # Interface
@@ -39,13 +34,14 @@ end
 
 # TODO convert function name to constructor
 function parse_model(json::AbstractDict, predecessor=nothing)
-    dict = Dict{String, Any}()
+    dict = Dict{String,AnElement}()
     foreach(json.notebook.cells) do cell
         @match cell.content.tag begin
             "object" => 
             push!(dict, cell.content[:id] => ObjectElem(cell.content[:name]))
             "morphism" => begin
-                name = if isempty(cell.content[:name])
+                name = 
+                if isempty(cell.content[:name])
                     if isnothing(cell.content[:over])
                         cell.content[:name]
                     else
@@ -64,7 +60,7 @@ function parse_model(json::AbstractDict, predecessor=nothing)
 end
 
 function parse_diagram(json::AbstractDict, predecessor=nothing)
-    dict = Dict{String, Any}()
+    dict = Dict{String,AnElement}()
     foreach(json.notebook.cells) do cell
         @match cell.content.tag begin
             "object" => 
@@ -79,8 +75,9 @@ function parse_diagram(json::AbstractDict, predecessor=nothing)
                 else
                     cell.content[:name]
                 end
+                label = @something emptyMaybe(subpart(predecessor, incident(predecessor, name, :eUuid), :elabel)) emptyMaybe(name) ["EMPTY"]
                 push!(dict, cell.content[:id] => 
-                HomElem(name, cell.content[:dom][:content], cell.content[:cod][:content]))
+                      HomElem(only(label), cell.content[:dom][:content], cell.content[:cod][:content]))
             end
         end
     end
@@ -93,14 +90,7 @@ SchLabeledGraph = BasicSchema([:E, :V],
              ,(:vUuid, :V, :UUID), (:eUuid, :E, :UUID)])
 @acset_type UUIDLabeledGraph(SchLabeledGraph, index=[:src,:tgt])
 
-
-modeljson = JSON3.read("test/json/model_dec.json")
-parsed_model = parse_json(modeljson)
-model = UUIDLabeledGraph(parsed_model)
-
-diagramjson = JSON3.read("test/json/diagram.json")
-parsed_diagram = parse_json(diagramjson, predecessor=model)
-diagram = UUIDLabeledGraph(parsed_diagram)
+emptyMaybe(x) = isempty(x) ? nothing : Some(x)
 
 function UUIDLabeledGraph(m::CCLModel)
     g = UUIDLabeledGraph{String, String}()
@@ -118,23 +108,14 @@ function UUIDLabeledGraph(m::CCLModel)
         add_part!(g, :V, vlabel=v.name, vUuid=k)
     end
     foreach(pairs(newhoms)) do (k,v)
-        # abstraction
-        src = let src = incident(g, v.dom, :vUuid)
-            if isempty(src)
-                nc += 1
-                add_part!(g, :V, vlabel="•$nc", vUuid=v.dom)
-            else
-                src
-            end
-        end
-        tgt = let tgt = incident(g, v.cod, :vUuid)
-            if isempty(tgt)
-                nc += 1
-                add_part!(g, :V, vlabel="•$nc", vUuid=v.cod)
-            else
-                tgt
-            end
-        end
+        src = @something emptyMaybe(incident(g, v.dom, :vUuid)) begin
+                             nc += 1
+                             add_part!(g, :V, vlabel="•$nc", vUuid=v.dom)
+                        end
+        tgt = @something emptyMaybe(incident(g, v.cod, :vUuid)) begin
+                             nc += 1
+                             add_part!(g, :V, vlabel="•$nc", vUuid=v.cod)
+                        end
         add_part!(g, :E, src=only(src), tgt=only(tgt), eUuid=k, elabel=v.name)
     end
     g
@@ -156,24 +137,23 @@ function UUIDLabeledGraph(m::CCLDiagram)
         add_part!(g, :V, vlabel=v.name, vUuid=k)
     end
     foreach(pairs(newhoms)) do (k,v)
-        # abstraction
-        src = let src = incident(g, v.dom, :vUuid)
-            if isempty(src)
-                nc += 1
-                add_part!(g, :V, vlabel="•$nc", vUuid=v.dom)
-            else
-                src
-            end
-        end
-        tgt = let tgt = incident(g, v.cod, :vUuid)
-            if isempty(tgt)
-                nc += 1
-                add_part!(g, :V, vlabel="•$nc", vUuid=v.cod)
-            else
-                tgt
-            end
-        end
+        src = @something emptyMaybe(incident(g, v.dom, :vUuid)) begin
+                             nc += 1
+                             add_part!(g, :V, vlabel="•$nc", vUuid=v.dom)
+                        end
+        tgt = @something emptyMaybe(incident(g, v.cod, :vUuid)) begin
+                             nc += 1
+                             add_part!(g, :V, vlabel="•$nc", vUuid=v.cod)
+                        end
         add_part!(g, :E, src=only(src), tgt=only(tgt), eUuid=k, elabel=v.name)
     end
     g
 end
+
+modeljson = JSON3.read("test/json/model_dec.json")
+parsed_model = parse_json(modeljson)
+model = UUIDLabeledGraph(parsed_model)
+
+diagramjson = JSON3.read("test/json/diagram.json")
+parsed_diagram = parse_json(diagramjson, predecessor=model)
+diagram = UUIDLabeledGraph(parsed_diagram)
